b3fe91f81a6b3e761d813c2a1ac86c1deb0b5f01
crh <muhe_crh@163.com>
2025-08-26 18:52:34 +0800

    1

---
+++ study_3/study_case/1_interface.go
@@ -0,0 +1,23 @@
+package study_case
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+// 定义一个结构体
+type Circle struct {
+	Radius float64
+ }
+
+ 
+func interfaceDefinition() {
+
+
+
+
+}
+
+func InterfaceCase() {
+
+}
